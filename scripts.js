eb7139635a0b1fd9c09aec9ad7623f6cf319e2c5
Manasaabevara <manasaabevara@gmail.com>
2024-06-02 15:56:50 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+﻿// scripts.js
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for subscribing! Stay tuned for new innovations.');
+});
+
